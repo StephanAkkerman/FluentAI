b5aacc03aacb0f0e8450479be16291167abfdd53
StephanAkkerman <stephan.akkerman@gmail.com>
2024-07-14 15:26:51 +0200

    Added code for verbal cue

---
+++ src/verbal_cue.py
@@ -0,0 +1,43 @@
+# pip install accelerate
+# pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
+
+torch.random.manual_seed(0)
+model = AutoModelForCausalLM.from_pretrained(
+    "microsoft/Phi-3-mini-4k-instruct",
+    device_map="cuda",
+    torch_dtype="auto",
+    trust_remote_code=True,
+)
+
+tokenizer = AutoTokenizer.from_pretrained("microsoft/Phi-3-mini-4k-instruct")
+
+messages = [
+    {"role": "system", "content": "You are a helpful AI assistant."},
+    {
+        "role": "user",
+        "content": "Write a short, catchy sentence that connects flashy and bottle. Also, the sentence must start with 'Imagine'.",
+    },
+    {
+        "role": "assistant",
+        "content": "Imagine a flashy bottle that stands out from the rest!",
+    },
+    {"role": "user", "content": "What about solving an 2x + 3 = 7 equation?"},
+]
+
+pipe = pipeline(
+    "text-generation",
+    model=model,
+    tokenizer=tokenizer,
+)
+
+generation_args = {
+    "max_new_tokens": 500,
+    "return_full_text": False,
+    "temperature": 0.0,
+    "do_sample": False,
+}
+
+output = pipe(messages, **generation_args)
+print(output[0]["generated_text"])
