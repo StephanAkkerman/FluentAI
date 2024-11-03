fa47bc3dc41e99b36021ab3e267eff55994a859c
Winston Lam <wing.lam@ing.com>
2024-11-03 16:29:58 +0100

    made simple loading screen

--- fluentai/front-end/src/pages/LoadingPage.tsx
+++ fluentai/front-end/src/pages/LoadingPage.tsx
@@ -1,16 +1,22 @@
-import React from 'react';
-import styles from '../styles/LoadingPageStyles';
+import React from "react";
+import styles from "../styles/LoadingPageStyles";
+
+// Import the logo image
+import logo from "../icons/logo.png";
 
 const LoadingPage: React.FC<{ isLoading: boolean }> = ({ isLoading }) => {
-    const wrapperClass = isLoading ? 'load-wrapper' : 'load-wrapper load-wrapper-completed';
+  const wrapperClass = isLoading
+    ? "load-wrapper"
+    : "load-wrapper load-wrapper-completed";
 
-    return (
-        <div className={wrapperClass} style={styles.loadWrapper}>
-            <div className="loading-text" style={styles.loadingText}>
-                Loading...
-            </div>
-        </div>
-    );
+  return (
+    <div className={wrapperClass} style={styles.loadWrapper}>
+      <img src={logo} alt="FluentAI Logo" style={styles.appLogo} />
+      <div className="loading-text" style={styles.loadingText}>
+        Loading...
+      </div>
+    </div>
+  );
 };
 
 export default LoadingPage;
