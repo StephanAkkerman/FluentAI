c76a1d58e384a452bea2c05428ae54c65bf9435a
StephanAkkerman <stephan.akkerman@gmail.com>
2024-11-19 20:10:18 +0100

    added some tests

--- fluentai/services/card_gen/mnemonic/imageability/predictions.py
+++ fluentai/services/card_gen/mnemonic/imageability/predictions.py
@@ -1,6 +1,3 @@
-# Load the imageability prediction model
-# Generate predictions for the IPA dataset
-# Upload it to StephanAkkerman/imageability-predictions
 import joblib
 import pandas as pd
 from huggingface_hub import hf_hub_download
