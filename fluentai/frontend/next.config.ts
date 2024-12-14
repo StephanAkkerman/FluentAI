630335052fdb548626a49b6303ce715fad1e0ae1
Tim Koornstra <89044870+TimKoornstra@users.noreply.github.com>
2024-12-14 13:44:03 +0100

    Update next.config.ts

--- fluentai/frontend/next.config.ts
+++ fluentai/frontend/next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
+  basePath: "/FluentAI",
+  images: {
+    unoptimized: true, // Required for GitHub Pages
+  },
 };
 
+export default nextConfig;
 export default nextConfig;
