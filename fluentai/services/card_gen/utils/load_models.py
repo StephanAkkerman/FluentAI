198bf2ad951897e1393302b00c9afe399b1fc4fe
StephanAkkerman <stephan.akkerman@gmail.com>
2024-12-19 21:38:44 +0100

    Added logging

--- fluentai/services/card_gen/utils/load_models.py
+++ fluentai/services/card_gen/utils/load_models.py
@@ -7,6 +7,7 @@ from sentence_transformers import SentenceTransformer
 from transformers import AutoModelForCausalLM, AutoTokenizer, T5ForConditionalGeneration
 
 from fluentai.services.card_gen.constants.config import config
+from fluentai.services.card_gen.utils.logger import logger
 
 
 def get_model_dir_name(model: str) -> str:
@@ -37,26 +38,31 @@ def download_all_models():
     # G2P model
     g2p_model = config.get("G2P").get("MODEL")
     if get_model_dir_name(g2p_model) not in downloaded_models:
+        logger.info(f"Downloading G2P model: {g2p_model}")
         clean(T5ForConditionalGeneration.from_pretrained(g2p_model, cache_dir="models"))
 
     # G2P tokenizer
     g2p_tokenizer = config.get("G2P").get("TOKENIZER")
     if get_model_dir_name(g2p_tokenizer) not in downloaded_models:
+        logger.info(f"Downloading G2P tokenizer: {g2p_tokenizer}")
         clean(AutoTokenizer.from_pretrained(g2p_tokenizer, cache_dir="models"))
 
     # LLM model
     llm_model = config.get("LLM").get("MODEL")
     if get_model_dir_name(llm_model) not in downloaded_models:
+        logger.info(f"Downloading LLM model: {llm_model}")
         clean(AutoModelForCausalLM.from_pretrained(llm_model, cache_dir="models"))
 
     # LLM tokenizer
     llm_tokenizer = config.get("LLM").get("TOKENIZER")
     if get_model_dir_name(llm_tokenizer) not in downloaded_models:
+        logger.info(f"Downloading LLM tokenizer: {llm_tokenizer}")
         clean(AutoTokenizer.from_pretrained(llm_tokenizer, cache_dir="models"))
 
     # Image gen
     image_gen_model = config.get("IMAGE_GEN").get("MODEL")
     if get_model_dir_name(image_gen_model) not in downloaded_models:
+        logger.info(f"Downloading image gen model: {image_gen_model}")
         clean(
             AutoPipelineForText2Image.from_pretrained(
                 image_gen_model, cache_dir="models"
@@ -66,6 +72,9 @@ def download_all_models():
     # Semantic similarity
     semantic_similarity_model = config.get("SEMANTIC_SIM").get("MODEL")
     if get_model_dir_name(semantic_similarity_model) not in downloaded_models:
+        logger.info(
+            f"Downloading semantic similarity model: {semantic_similarity_model}"
+        )
         clean(
             SentenceTransformer(
                 semantic_similarity_model, trust_remote_code=True, cache_folder="models"
