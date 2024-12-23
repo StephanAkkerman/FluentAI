7210adcbf8a7ab1aab820bc64254839b571fd887
StephanAkkerman <stephan.akkerman@gmail.com>
2024-12-23 18:52:58 +0100

    Improve custom mappings + add check

--- fluentai/services/card_gen/utils/lang_codes.py
+++ fluentai/services/card_gen/utils/lang_codes.py
@@ -1,5 +1,10 @@
 import pycountry
 
+from fluentai.services.card_gen.constants.languages import (
+    G2P_LANGCODES,
+    TRANSLATE_LANGCODES,
+)
+
 
 def map_language_code(input_code: str) -> str:
     """
@@ -16,6 +21,10 @@ def map_language_code(input_code: str) -> str:
     ------
         ValueError: If the language code cannot be mapped.
     """
+    # Check if the code is supported in
+    if input_code in TRANSLATE_LANGCODES.values():
+        return input_code
+
     # Handle special cases
     # https://py-googletrans.readthedocs.io/en/latest/#googletrans-languages
     special_mappings = {
@@ -40,6 +49,18 @@ def map_language_code(input_code: str) -> str:
         "por-br": "pt",  # Portuguese (Brazil)
         "por-po": "por",  # Portuguese (Portugal)
         "dut": "nl",  # Dutch
+        "bur": "my",  # Burmese
+        "cze": "cs",  # Czech
+        "geo": "ka",  # Georgian
+        "ger": "de",  # German
+        "gre": "el",  # Greek
+        "grc": "el",  # Ancient Greek
+        "mac": "mk",  # Macedonian
+        "tts": "th",  # Thai
+        "slo": "sk",  # Slovak
+        "ice": "is",  # Icelandic
+        "gle": "ga",  # Irish
+        "nob": "no",  # Norwegian (Bokmål)
     }
 
     if input_code in special_mappings:
@@ -48,4 +69,18 @@ def map_language_code(input_code: str) -> str:
     # Attempt to get ISO 639-1 code using pycountry
     language = pycountry.languages.get(alpha_3=input_code.lower())
     if language and hasattr(language, "alpha_2"):
-        return language.alpha_2
+        # Check if the code is in the list of supported languages
+        if language.alpha_2 in TRANSLATE_LANGCODES.values():
+            return language.alpha_2
+
+
+if __name__ == "__main__":
+    # Try mapping all G2p language codes
+    for lang, code in G2P_LANGCODES.items():
+        mapped = map_language_code(code)
+        if mapped is None:
+            print(f"Failed to map {code} ({lang})")
+        else:
+            # Check if the code is in the list of supported languages
+            if mapped not in TRANSLATE_LANGCODES.values():
+                print(f"Unsupported language code: {code} ({lang})")
