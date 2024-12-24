8385afeecb20c29e7834998ded4ce183abc1ed8d
StephanAkkerman <stephan.akkerman@gmail.com>
2024-12-24 14:29:54 +0100

    Added sana model support

--- fluentai/services/card_gen/imagine/image_gen.py
+++ fluentai/services/card_gen/imagine/image_gen.py
@@ -9,89 +9,76 @@ from fluentai.services.card_gen.constants.config import config
 from fluentai.services.card_gen.utils.logger import logger
 
 
-def generate_img(
-    prompt: str = "A flashy bottle that stands out from the other bottles.",
-    word1: str = "flashy",
-    word2: str = "bottle",
-):
-    """
-    Generate an image from a text prompt using a text-to-image model.
+class ImageGen:
+    def __init__(self):
+        # Get all config values
+        self.model = config.get("IMAGE_GEN", {}).get("MODEL", "stabilityai/sdxl-turbo")
+        self.model_name = self.model.split("/")[-1]
 
-    Parameters
-    ----------
-    prompt : str, optional
-        The prompt to give to the model, by default "A flashy bottle that stands out from the other bottles."
-    word1 : str, optional
-        The first word of mnemonic, by default "flashy"
-    word2 : str, optional
-        The second word, by default "bottle"
-    """
-    model = config.get("IMAGE_GEN", {}).get("MODEL", "stabilityai/sdxl-turbo")
-    model_name = model.split("/")[-1]
+        self.output_dir = Path(
+            config.get("IMAGE_GEN", {}).get("OUTPUT_DIR", "output")
+        ).resolve()
+        os.makedirs(self.output_dir, exist_ok=True)
 
-    pipe = AutoPipelineForText2Image.from_pretrained(
-        model,
-        torch_dtype=torch.float16,
-        variant="fp16",
-        cache_dir="models",
-    )
+        self.image_gen_params = config.get("IMAGE_GEN", {}).get("PARAMS", {})
 
-    # Check if cuda is enabled
-    if torch.cuda.is_available():
-        logger.debug("CUDA is available. Moving the t2i pipeline to CUDA.")
-        pipe.to("cuda")
-    else:
-        logger.info("CUDA is not available. Running the image gen pipeline on CPU.")
+        pipe_func = self._get_pipe_func()
+        self.pipe = pipe_func.from_pretrained(
+            self.model,
+            torch_dtype=torch.float16,
+            variant="fp16",
+            cache_dir="models",
+        )
 
-    logger.info(f"Generating image for prompt: {prompt}")
+    def _get_pipe_func(self):
+        if "sana" in self.model_name.lower():
+            return SanaPipeline
+        else:
+            return AutoPipelineForText2Image
 
-    # Get the parameters for image generation from config
-    image_gen_params = config.get("IMAGE_GEN", {}).get("PARAMS", {})
-    image = pipe(prompt=prompt, **image_gen_params).images[0]
+    def generate_img(
+        self,
+        prompt: str = "A flashy bottle that stands out from the other bottles.",
+        word1: str = "flashy",
+        word2: str = "bottle",
+    ):
+        """
+        Generate an image from a text prompt using a text-to-image model.
 
-    # Get the output directory from config
-    output_dir = Path(config.get("IMAGE_GEN", {}).get("OUTPUT_DIR", "output")).resolve()
-    os.makedirs(output_dir, exist_ok=True)
+        Parameters
+        ----------
+        prompt : str, optional
+            The prompt to give to the model, by default "A flashy bottle that stands out from the other bottles."
+        word1 : str, optional
+            The first word of mnemonic, by default "flashy"
+        word2 : str, optional
+            The second word, by default "bottle"
+        """
+        file_path = self.output_dir / f"{word1}_{word2}_{self.model_name}.png"
 
-    file_path = output_dir / f"{word1}_{word2}_{model_name}.png"
-    logger.info(f"Saving image to: {file_path}")
+        # Check if cuda is enabled
+        if torch.cuda.is_available():
+            logger.debug("CUDA is available. Moving the t2i pipeline to CUDA.")
+            self.pipe.to("cuda")
+        else:
+            logger.info("CUDA is not available. Running the image gen pipeline on CPU.")
 
-    image.save(file_path)
+        logger.info(f"Generating image for prompt: {prompt}")
 
-    if config.get("IMAGE_GEN", {}).get("DELETE_AFTER_USE", True):
-        logger.debug("Deleting the VerbalCue model to free up memory.")
-        del pipe
-        gc.collect()
-        torch.cuda.empty_cache()
+        image = self.pipe(prompt=prompt, **self.image_gen_params).images[0]
 
-    return file_path
+        logger.info(f"Saving image to: {file_path}")
+
+        image.save(file_path)
+
+        if config.get("IMAGE_GEN", {}).get("DELETE_AFTER_USE", True):
+            logger.debug("Deleting the VerbalCue model to free up memory.")
+            del self.pipe
+            gc.collect()
+            torch.cuda.empty_cache()
+
+        return file_path
 
 
 if __name__ == "__main__":
-    # generate_img()
-    pipe = SanaPipeline.from_pretrained(
-        "Efficient-Large-Model/Sana_600M_512px_diffusers",
-        torch_dtype=torch.float16,
-        variant="fp16",
-        cache_dir="models",
-    )
-
-    # Check if cuda is enabled
-    if torch.cuda.is_available():
-        logger.debug("CUDA is available. Moving the t2i pipeline to CUDA.")
-        pipe.to("cuda")
-    else:
-        logger.info("CUDA is not available. Running the image gen pipeline on CPU.")
-
-    image = pipe(
-        prompt="A flashy bottle that stands out from the other bottles."
-    ).images[0]
-
-    # Get the output directory from config
-    output_dir = Path(config.get("IMAGE_GEN", {}).get("OUTPUT_DIR", "output")).resolve()
-    os.makedirs(output_dir, exist_ok=True)
-
-    file_path = output_dir / f"test.png"
-    logger.info(f"Saving image to: {file_path}")
-
-    image.save(file_path)
+    ImageGen().generate_img()
