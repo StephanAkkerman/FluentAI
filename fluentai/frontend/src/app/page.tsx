ebfe422bd66e9f48866011e3bcc8ff90b788251c
TimKoornstra <tim.koornstra@gmail.com>
2024-12-29 10:34:28 +0100

    Feat: #174 show API status bar when loading page

--- fluentai/frontend/src/app/page.tsx
+++ fluentai/frontend/src/app/page.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import CardGenerator from "../components/CardGenerator";
 import Flashcard from "../components/Flashcard";
 import { Card } from "@/interfaces/CardInterfaces";
+import StatusChecker from "@/components/StatusChecker";
 
 export default function Home() {
   const [card, setCard] = useState<Card | null>(null);
@@ -22,22 +23,25 @@ export default function Home() {
   };
 
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 items-start min-h-screen">
-      <div className="flex gap-12 flex-col">
-        <CardGenerator
-          onCardCreated={setCard}
-          onLoading={setLoading}
-          onError={setError}
-          onWordChange={setCurrentWord}
-        />
-        {error && <p className="text-red-500 font-medium mt-4">{error}</p>}
-      </div>
-      <div className="flex items-center justify-center">
-        <Flashcard
-          card={card || defaultCard}
-          isLoading={loading}
-          showFront={!!card}
-        />
+    <div className="min-h-screen flex flex-col gap-12">
+      <StatusChecker />
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 items-start">
+        <div className="flex gap-12 flex-col">
+          <CardGenerator
+            onCardCreated={setCard}
+            onLoading={setLoading}
+            onError={setError}
+            onWordChange={setCurrentWord}
+          />
+          {error && <p className="text-red-500 font-medium mt-4">{error}</p>}
+        </div>
+        <div className="flex items-center justify-center">
+          <Flashcard
+            card={card || defaultCard}
+            isLoading={loading}
+            showFront={!!card}
+          />
+        </div>
       </div>
     </div>
   );
