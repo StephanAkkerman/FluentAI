75ac8cd6f329109e6a097b39276183b5fdf0cf88
Stephan Akkerman <stephan.akkerman@gmail.com>
2025-04-05 21:49:35 +0200

    Try out different gen args

--- backend/fluentai/services/mnemonic/word2mnemonic2.py
+++ backend/fluentai/services/mnemonic/word2mnemonic2.py
@@ -8,14 +8,27 @@ from transformers import (
 
 from fluentai.constants.config import config
 from fluentai.logger import logger
-from fluentai.utils.model_mem import manage_memory
 
 
 class MnemonicGen:
     def __init__(self):
         self.config = config.get("LLM")
 
-        self.model_name = "Qwen/Qwen2.5-7B-Instruct"
+        self.model_name = (
+            "microsoft/Phi-3-mini-4k-instruct"
+            # "Qwen/Qwen2.5-7B-Instruct"
+        )
+
+        # use recommended settings for phi-3.5
+        self.generation_args = {
+            "max_new_tokens": 512,
+            "do_sample": True,
+            "num_beams": 1,
+            "top_k": 50,
+            "top_p": 0.95,
+            "return_full_text": False,
+            "temperature": 0.6,
+        }
 
         self.model = AutoModelForCausalLM.from_pretrained(
             self.model_name,
@@ -40,20 +53,25 @@ class MnemonicGen:
         self.messages = [
             {
                 "role": "system",
-                "content": "You are Qwen, created by Alibaba Cloud. You are a helpful assistant.",
+                "content": "You are a helpful assistant that generates mnemonics for foreign language vocabulary words. The mnemonic should sound very similar to the foreign word.  Use common English words or short phrases.",
             }
         ]
 
     def generate_mnemonic(self, language: str = "Indonesian", word: str = "Kucing"):
         final_message = {
             "role": "user",
-            "content": f"Think of a mnemonic to remember the {language} word {word}. Think of an English word or 2 words that sound similar to how Kucing would be pronounced in Indonesian. Also consider that the mnemonic should be an easy to imagine word and a word that is commonly used. Give a list of 10 mnemonic options based on these criteria.",
+            "content": f"""Think of a mnemonic to remember the {language} word {word}. 
+Think of an English word or a combination of 2 words that sound similar to how {word} would be pronounced in {language}. 
+Also consider that the mnemonic should be an easy to imagine word and a word that is commonly used. 
+Do not simply translate the word, the mnemonic should be a *memory aid* based on sound, not a translation.
+Give a list of 10 mnemonic options based on these criteria. 
+Give your output in JSON format.""",
         }
 
         # For some reason using tokenizer.apply_chat_template() here causes weird output
         input = self.messages + [final_message]
         print(input)
-        output = self.pipe(input)
+        output = self.pipe(input, **self.generation_args)
         response = output[0]["generated_text"]
         logger.debug(f"Generated cue: {response}")
         return response
